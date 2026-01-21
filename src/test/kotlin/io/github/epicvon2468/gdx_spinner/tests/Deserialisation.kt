ba4e3ff56dc56b391caf3180c99c71a36f5add69
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-22 00:41:02 +1100

    Deserialisation base

--- src/test/kotlin/io/github/epicvon2468/gdx_spinner/tests/Deserialisation.kt
+++ src/test/kotlin/io/github/epicvon2468/gdx_spinner/tests/Deserialisation.kt
@@ -1,11 +1,23 @@
 package io.github.epicvon2468.gdx_spinner.tests
 
+import Yarn.YarnSpinner
+import Yarn.operand
+
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Deserialisation {
 
 	@Test
 	fun deserialise() {
-		// TODO: Test deserialisation of built bytecode (setup YarnSpinner CLI first)
+		val program: YarnSpinner.Program = YarnSpinner.Program.parseFrom(
+			this::class.java.getResourceAsStream("/scripts/out/TryYarnSpinner/TryYarnSpinner.yarnc")
+		)
+		assertEquals(
+			operand {
+				floatValue = 0.0f
+			},
+			program.initialValuesMap[$$"$gold"]!!
+		)
 	}
 }
